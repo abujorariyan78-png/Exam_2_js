2d954ba457dad98d20e31f9159717e9a26244cfb
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-08-25 08:53:04 +0600

    Exam_2_js

---
+++ 16 switch statement.js
@@ -0,0 +1,20 @@
+var today="A"
+switch(today){
+    case A:
+        console.log("Hey i'm a studnet")
+        break;
+        case B:
+            console.log("B is the second letter of the alphabet")
+            break;
+            case C:
+        console.log("Hey i'm a studnet")
+        break;
+        case D:
+        console.log("Hey i'm a studnet")
+        break;
+        case E:
+        console.log("Hey i'm a studnet")
+        break;
+}
+
+// Mama ai tha onak chasta korci but pari nai 
